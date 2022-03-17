bb58d18de199d0e789f1621c39838f61704e5e9b
Edward Casanova <cb.inv7613@gmail.com>
2022-03-16 20:03:57 -0600

    sorting

--- sorting.ts
+++ sorting.ts
@@ -65,12 +65,47 @@ class CArray {
       }
     }
   }
+
+  selectionSort() {
+    let min;
+
+    //Only focus on the first 8 elements
+    for (let outer = 0; outer <= this.dataStore.length - 2; outer++) {
+      min = outer;
+
+      //Focus on the elements following (i+1) keeping a difference of 1 to stay ahead
+      for (let inner = outer + 1; inner <= this.dataStore.length - 1; inner++) {
+        //Set the min
+        if (this.dataStore[inner] < this.dataStore[min]) {
+          min = inner;
+        }
+      }
+
+      this.swap(outer, min);
+    }
+  }
+
+  insertionSort() {
+    let inner, temp;
+
+    for (let outer = 1; outer <= this.dataStore.length - 1; ++outer) {
+      temp = this.dataStore[outer];
+      inner = outer;
+
+      while (inner > 0 && this.dataStore[inner - 1] >= temp) {
+        this.dataStore[inner] = this.dataStore[inner - 1];
+        --inner;
+      }
+
+      this.dataStore[inner] = temp;
+    }
+  }
 }
 
 const myNums = new CArray(10);
 myNums.setData();
 console.log(myNums.toString());
 
-myNums.bubbleSort();
+myNums.insertionSort();
 
 console.log(myNums.toString());
